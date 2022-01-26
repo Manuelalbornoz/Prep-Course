b6caf2835d265591f13a47b81eb235deb8affb96
unknown <manuelalbornoz9@gmail.com>
2022-01-26 03:28:43 -0300

    tarea terminada

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -318,15 +318,15 @@ function esVocal(letra){
   
   if(letra.length > 1)
   {
-    return "Dato incorrecto"
+    return "Dato incorrecto";
   }
   if(letra === 'a' || letra === 'e' || letra === 'i' || letra === 'o' || letra === 'u')
   { 
-    return "Es vocal"
+    return "Es vocal";
   }
   else
   {
-    return "Dato incorrecto"
+    return "Dato incorrecto";
   }
 }
 
